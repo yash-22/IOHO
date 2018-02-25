64fec75cf4cd9f3340234220b3735d3d5b65a30a
VaibhavBajaj <vbajajpersonal@gmail.com>
2018-02-24 22:44:57 -0600

    Changed DbConnect to store title and date as well

--- dbconnect.py
+++ dbconnect.py
@@ -2,19 +2,19 @@ from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 from config import CLOUDINARY
 from cloudinary import uploader
 import cloudinary
+import datetime
 
 class DbConnect:
-	target = "temp.mp4"
+    target = "temp.mp4"
 
-	def clip(self, filename, title, timestamp, start, end):
-		ffmpeg_extract_subclip(filename, start, end, targetname = self.target)
-		self.title = title
-		self.timestamp = timestamp
+    def clip(self, filename,  start, end):
+        ffmpeg_extract_subclip(filename, start, end, targetname = self.target)
 
-	def push_to_db(self):
-		cloudinary.config(
+    def push_to_db(self, title):
+        cloudinary.config(
           cloud_name = CLOUDINARY['cloud_name'],  
           api_key = CLOUDINARY['api_key'],  
           api_secret = CLOUDINARY['api_secret']
         )
-		cloudinary.uploader.upload(self.target, resource_type = "video")
+        id = title + ", " + datetime.datetime.now().strftime("%H:%M:%S %m-%d-%Y")
+        cloudinary.uploader.upload(self.target, resource_type="video", public_id=id)
--- tests/dbconnect_test.py
+++ tests/dbconnect_test.py
@@ -1,12 +1,12 @@
 import datetime
 from dbconnect import DbConnect
 
-filename = "FILENAME.mp4"
+filename = "50thBirthday.mp4"
 crime_title = "Amazing video :)"
 t1 = 27
 t2 = 32
 
 if __name__ == '__main__':
     connection = DbConnect()
-    connection.clip(filename, crime_title, datetime.datetime.now(), t1, t2)
-    connection.push_to_db()
+    connection.clip(filename, t1, t2)
+    connection.push_to_db(crime_title)
